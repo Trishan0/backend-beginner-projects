59b58fa63f2c8b8a750249b46348aaac1d6cb48e
Trishan <sanjanatrishan@gmail.com>
2025-04-01 10:59:33 +0530

    Add dynamic weather data retrieval using external API
    Add error handling for missing or invalid location parameters.

--- weather-api/api/api.py
+++ weather-api/api/api.py
@@ -1,16 +1,37 @@
 from flask import request, Flask, jsonify
+from dotenv import load_dotenv
+import requests
+import os
 
+load_dotenv()
 api = Flask(__name__)
+API_KEY = os.getenv('API_KEY')
+
+
+def get_weather(location):
+    url = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?unitGroup=us&include=days&key={API_KEY}&contentType=json'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
 
 @api.route('/weather', methods=['GET'])
-def getweather():
-    weather_data = {
-        "location": "Colombo",
-        "temperature": "30°C",
-        "condition": "Sunny",
-        "humidity": "70%"
-    }
-    return jsonify(weather_data)
+def weather():
+    location = request.args.get('location')
+
+    if not location:
+        return jsonify({"error": "location parameter is required"}), 400
+
+    weather_data = get_weather(location)
+
+    if weather_data:
+        return jsonify({"data": weather_data})
+
+    return jsonify({"error": "Failed to fetch weather data"}), 500
+
 
 if __name__ == '__main__':
     api.run(debug=True, port=5000)
