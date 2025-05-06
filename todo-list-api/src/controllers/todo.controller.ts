d40a91737e9f63cc0247cbd30c0fc39288e8e3e1
Trishan <sanjanatrishan@gmail.com>
2025-05-06 23:03:58 +0530

    Fix syntax issues and improve error handling in controller

--- todo-list-api/src/controllers/todo.controller.ts
+++ todo-list-api/src/controllers/todo.controller.ts
@@ -1,18 +1,20 @@
-import { Request, Response } from "express";
-import { createTodo } from "../services";
+import {Request, Response} from "express";
+import {createTodo} from "../services";
 
-export const createTodoController = async (req: Request, res: Response)=>{
+export const createTodoController = async (req: Request, res: Response) => {
     try {
         const {title, description, date, status} = req.body;
-        const newTodo = await createTodo({title, description, date, status})
+
+        const newTodo = await createTodo({title, description, date, status});
+
         return res.status(201).json({
             success: true,
             data: newTodo
-        })
-    } catch (e) {
+        });
+    } catch (error) {
         return res.status(500).json({
             success: false,
-            message: "Server Error", e
-        })
+            message: "Server Error", error
+        });
     }
-}
+};
