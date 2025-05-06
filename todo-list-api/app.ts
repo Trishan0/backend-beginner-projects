f3426dcb11b88bcd6d39042bb574b11c93d980b3
Trishan <sanjanatrishan@gmail.com>
2025-05-06 23:31:14 +0530

    Refactor app initialization and fix routing setup

--- todo-list-api/app.ts
+++ todo-list-api/app.ts
@@ -1,18 +1,18 @@
 import express from 'express';
 import { connectDB } from "./src/database/db";
-import {CONFIG} from "./src/config";
-import {router} from "./src/routes";
+import { CONFIG } from "./src/config";
+import { todoRouter } from "./src/routes";
 
 const app = express();
 
-connectDB().then(()=> {
+app.use(express.json());
+app.use('/api', todoRouter);
+
+connectDB().then(() => {
     app.listen(CONFIG.PORT, () => {
         console.log(`Server running at http://localhost:${CONFIG.PORT}`);
-    })
-}).catch(err=>{
+    });
+}).catch(err => {
     console.error('Failed to connect to database', err);
     process.exit(1);
-})
-
-app.use(express.json());
-app.use('/api', router);
+});
