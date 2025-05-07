881aa6675804e13b2aa52cd5420f42588d53f34f
Trishan <sanjanatrishan@gmail.com>
2025-05-08 02:08:57 +0530

    Add route to fetch all todos

--- todo-list-api/src/routes/todo.routes.ts
+++ todo-list-api/src/routes/todo.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { createTodoController } from "../controllers";
+import { createTodoController, viewTodoController } from "../controllers";
 
 const todoRouter = Router();
 
 todoRouter.post("/todos", createTodoController);
-
+todoRouter.get("/todos", viewTodoController);
 export { todoRouter };
