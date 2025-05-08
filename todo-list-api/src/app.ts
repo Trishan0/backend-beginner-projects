72a28f9085180d6c65914d7750c18503a5bedb30
Trishan <sanjanatrishan@gmail.com>
2025-05-09 01:43:45 +0530

    Add authentication routes to the application

--- todo-list-api/src/app.ts
+++ todo-list-api/src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { connectDB } from "./database/db";
 import { CONFIG } from "./config";
 import { todoRouter } from "./routes";
+import { authRouter } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use('/api', todoRouter);
+app.use('/api/auth', authRouter);
 
 connectDB().then(() => {
     app.listen(CONFIG.PORT, () => {
