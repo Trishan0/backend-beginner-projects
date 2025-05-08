dea176d6e75bd4f51525c1e07735c641b772cad1
Trishan <sanjanatrishan@gmail.com>
2025-05-08 14:05:19 +0530

    Add route to get a todo by its ID

--- todo-list-api/src/routes/todo.routes.ts
+++ todo-list-api/src/routes/todo.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { createTodoController, viewTodoController } from "../controllers";
+import { createTodoController, viewTodoController,viewTodoByIdController } from "../controllers";
 
 const todoRouter = Router();
 
 todoRouter.post("/todos", createTodoController);
 todoRouter.get("/todos", viewTodoController);
+todoRouter.get("/todos/:id", viewTodoByIdController);
 export { todoRouter };
