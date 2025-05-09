0ab1902ad6455a9a0ed3d9dae246817d6e0eae2f
Trishan <sanjanatrishan@gmail.com>
2025-05-10 01:29:01 +0530

    Add authentication middleware to todo endpoints

--- todo-list-api-js/src/routes/todo.routes.js
+++ todo-list-api-js/src/routes/todo.routes.js
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 import {createTodoController, getAllTodoController, getTodoByIdController, updateTodoByIdController, deleteTodoByIdController} from "../controllers/todo.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const todoRouter = Router();
 
-todoRouter.post("/todos", createTodoController);
-todoRouter.get("/todos", getAllTodoController);
-todoRouter.get("/todos/:id", getTodoByIdController);
-todoRouter.put("/todos/:id", updateTodoByIdController);
-todoRouter.delete("/todos/:id", deleteTodoByIdController);
+todoRouter.post("/todos", authMiddleware, createTodoController);
+todoRouter.get("/todos", authMiddleware, getAllTodoController);
+todoRouter.get("/todos/:id", authMiddleware, getTodoByIdController);
+todoRouter.put("/todos/:id", authMiddleware, updateTodoByIdController);
+todoRouter.delete("/todos/:id", authMiddleware, deleteTodoByIdController);
 export {todoRouter}
