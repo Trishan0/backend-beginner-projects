dca50b75714838f7d71bf8a31a6d6d22f621588a
Trishan <sanjanatrishan@gmail.com>
2025-05-09 14:05:38 +0530

    Add auto-incrementing ID field to Todo model

--- todo-list-api-js/src/models/todo.model.js
+++ todo-list-api-js/src/models/todo.model.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
+import AutoIncrementFactory from 'mongoose-sequence';
+import {CONFIG} from "../config/config.js";
 
+const connection = mongoose.createConnection(CONFIG.MONGODB_URI);
+const AutoIncrement = AutoIncrementFactory(connection);
 const todoSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -14,4 +18,5 @@ const todoSchema = new mongoose.Schema({
 
 }, { timestamps: true });
 
+todoSchema.plugin(AutoIncrement, { inc_field: 'id' });
 export const Todo = new mongoose.model('Todo', todoSchema);
