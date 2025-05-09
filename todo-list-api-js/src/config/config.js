72c3cc9ac1c1fa4da2df7854f52a0d630696dbde
Trishan <sanjanatrishan@gmail.com>
2025-05-10 01:27:11 +0530

    Add JWT_SECRET_KEY to configuration

--- todo-list-api-js/src/config/config.js
+++ todo-list-api-js/src/config/config.js
@@ -4,5 +4,6 @@ dotenv.config()
 
 export const CONFIG = {
     PORT:process.env.PORT || 3000,
-    MONGODB_URI:process.env.MONGODB_URI
+    MONGODB_URI:process.env.MONGODB_URI,
+    JWT_SECRET_KEY:process.env.JWT_SECRET_KEY
 }
