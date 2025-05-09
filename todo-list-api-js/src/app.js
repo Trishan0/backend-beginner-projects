70d0882430b48b32383e771488f97076392864b6
Trishan <sanjanatrishan@gmail.com>
2025-05-10 01:29:27 +0530

    Add authentication routes to the API

--- todo-list-api-js/src/app.js
+++ todo-list-api-js/src/app.js
@@ -1,17 +1,19 @@
 import express from 'express';
-import { connectDB } from "./database/db.js";
+import {connectDB} from "./database/db.js";
 import {CONFIG} from "./config/config.js";
 import {todoRouter} from "./routes/todo.routes.js";
+import {authRouter} from "./routes/auth.routes.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/api", todoRouter);
+app.use("/api/auth", authRouter);
 
-connectDB().then(()=>{
-    app.listen(CONFIG.PORT, ()=>{
+connectDB().then(() => {
+    app.listen(CONFIG.PORT, () => {
         console.log(`Server running at http://localhost:${CONFIG.PORT}`);
     });
-}).catch(err=>{
+}).catch(err => {
     console.error("Failed to connect to database", err);
 })
