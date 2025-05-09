cd08f6fedfed859613c7e4dc2cc877657c33c46f
Trishan <sanjanatrishan@gmail.com>
2025-05-09 12:53:40 +0530

    Add createTodoController to handle todo creation API

---
+++ todo-list-api-js/src/controllers/todo.controller.js
@@ -0,0 +1,28 @@
+import {createTodoService} from "../services/todo.services.js";
+
+const createTodoController = async (req, res)=>{
+    try{
+        const {title, description} = req.body;
+        if (!title) {
+            res.status(400).json({
+                success: false,
+                message: "Title is required"
+            });
+        }
+        const newTodo = await createTodoService({title, description})
+        res.status(201).json({
+            success: true,
+            data: newTodo
+        });
+    }catch (error) {
+        console.error("Error in createTodoController:", error);
+
+        res.status(500).json({
+            success: false,
+            message: "Server Error",
+            error: error instanceof Error ? error.message : "An unexpected error occurred"
+        });
+    }
+}
+
+export {createTodoController}
