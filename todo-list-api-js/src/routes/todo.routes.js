a62c2115d375becc27efd5d07e26eece800c7e5e
Trishan <sanjanatrishan@gmail.com>
2025-05-09 15:00:28 +0530

    Add update and delete routes for todos

--- todo-list-api-js/src/routes/todo.routes.js
+++ todo-list-api-js/src/routes/todo.routes.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
-import {createTodoController, getAllTodoController, getTodoByIdController} from "../controllers/todo.controller.js";
+import {createTodoController, getAllTodoController, getTodoByIdController, updateTodoByIdController, deleteTodoByIdController} from "../controllers/todo.controller.js";
 
 const todoRouter = Router();
 
 todoRouter.post("/todos", createTodoController);
 todoRouter.get("/todos", getAllTodoController);
 todoRouter.get("/todos/:id", getTodoByIdController);
+todoRouter.put("/todos/:id", updateTodoByIdController);
+todoRouter.delete("/todos/:id", deleteTodoByIdController);
 export {todoRouter}
